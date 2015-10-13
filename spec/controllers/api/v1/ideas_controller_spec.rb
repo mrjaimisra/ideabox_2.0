460b637c182d476026dcbf85bf9760fd61de7ddc
mrjaimisra <misrajai01@gmail.com>
2015-10-13 15:06:52 -0600

    Write spec for api/v1/ideas#create action Add javascript method for creating new idea in the dom and posting it to index Add html form to index page for creating new idea in the DOM Add create action to api/v1/ideas controller

--- spec/controllers/api/v1/ideas_controller_spec.rb
+++ spec/controllers/api/v1/ideas_controller_spec.rb
@@ -3,10 +3,15 @@ require 'rails_helper'
 RSpec.describe Api::V1::IdeasController, type: :controller do
   let!(:idea) { Idea.create(title: "Idea", body: "This is the best idea")}
 
-  it "receives a successful response" do
-    get 'index', format: :json
+  it "receives a successful response when requesting @ideas in json" do
+    get :index, format: :json
+
+    data = JSON.parse(response.body, symbolize_names: true)
 
     expect(response.status).to eq(200)
+    expect(data.first[:id]).to eq(idea.id)
+    expect(data.first[:title]).to eq(idea.title)
+    expect(data.first[:body]).to eq(idea.body)
   end
 
   it "renders the index template" do
@@ -14,8 +19,8 @@ RSpec.describe Api::V1::IdeasController, type: :controller do
     expect(response).to render_template("index")
   end
 
-  it "assigns @ideas" do
-    get :index
-    expect(assigns(:ideas)).to eq([idea])
+  it "creates a new idea" do
+    expect { post :create, format: :json, idea: { title: "Second idea", body: "This is another idea" }
+    }.to change(Idea,:count)
   end
 end
